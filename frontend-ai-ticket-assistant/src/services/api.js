81260318cc7bd41720f044b456654871120e3a3c
nikhil kumar <118626142+9ikhil@users.noreply.github.com>
2025-08-17 20:37:12 +0530

    deployment backend services
    deployment backend services

--- frontend-ai-ticket-assistant/src/services/api.js
+++ frontend-ai-ticket-assistant/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_URL || "https://ai-agent-ticket-assistent.onrender.com";
 
 // Create axios instance
 const api = axios.create({
@@ -181,3 +181,4 @@ export const apiUtils = {
 
 
 export default api;
+
